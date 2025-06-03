ad9c9e5c3d1df65d5100ac94333fc5e13d9d5ca0
sharm <sharmainerioveros@gmail.com>
2025-06-04 06:37:32 +0800

    Update App.tsx
    - add dark mode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
-import { AppShell, Button, Group, Title } from '@mantine/core'
-import reactlogo from '../assets/react.svg'
-import Sidebar from './Sidebar';
+import { AppShell, Button, Group, Title } from "@mantine/core";
+import reactlogo from "../assets/react.svg";
+import Sidebar from "./Sidebar";
+import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
 const App: React.FC = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === "dark";
   return (
     <AppShell
       header={{ height: 70 }}
@@ -14,11 +18,22 @@ const App: React.FC = () => {
           <img src={reactlogo} alt="React Logo" style={{ height: 40 }} />
           <Title order={2}>Personal Expense Tracker</Title>
         </Group>
+        <ActionIcon
+          variant="outline"
+          color={isDark ? "yellow" : "blue"}
+          onClick={() => toggleColorScheme()}
+          style={{ position: "absolute", right: 20, top: 20 }}
+        >
+          {isDark ? (
+            <IconSun style={{ width: 18, height: 18 }} />
+          ) : (
+            <IconMoonStars style={{ width: 18, height: 18 }} />
+          )}
+        </ActionIcon>
       </AppShell.Header>
       <AppShell.Navbar p="md">
         <Sidebar />
       </AppShell.Navbar>
-      
     </AppShell>
   );
 };
